088f66cd94c7fb036d6e54d4791b688219ecc6c5
mzparulina <pauline.revilla@gmail.com>
2023-01-11 16:55:09 -0500

    Add a test and refactored the code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,25 @@
 const request = require('request');
-const breed = process.argv[2];
-const domain = "https://api.thecatapi.com/v1/breeds/search?name=" + breed;
 
-request(domain, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (data.length > 0) {
-    console.log(data[0].description);
-  } else {
-    console.log('Breed not found');
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  const api = "https://api.thecatapi.com/v1/breeds/search?name=" + breedName;
+  request(api, (error, response, body) => {
+    const data = JSON.parse(body);
+
+    if (error) { //check for error
+      callback(error);
+      return;
+    }
+
+    if (data.length === 0) { // make sure cat exists
+      callback('That cat does not exist.', null);
+      return;
+    }
+    
+    if (data[0] !== undefined) { // make sure it has an object
+      callback(error, data[0].description);
+    }
+    
+  });
+};
+
+module.exports = { fetchBreedDescription };
